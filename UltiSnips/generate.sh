7e3dfb8b69feb649986d4ce13b7036a3b56dee45
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2019-09-03 07:24:55 -0300

    UltiSnips works with Ansible installed using homebrew

---
+++ UltiSnips/generate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+cd "$(dirname "$0")"
+
+ANSIBLE_MODULE_LOCATION="$(ansible --version | grep "ansible python module location" | sed -E 's/ +ansible python module location += +//g')"
+PYTHONPATH="$(dirname "$ANSIBLE_MODULE_LOCATION")"
+PYTHON_VERSION=$(basename "$(dirname "$PYTHONPATH")")
+
+export PYTHONPATH
+exec "$PYTHON_VERSION" generate.py
