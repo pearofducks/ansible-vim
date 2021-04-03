370589e6fc5840cd37237038775849acd1b6fb81
Pablo Escobar <pescobar001@gmail.com>
2021-04-03 10:13:42 +0200

    only process user modules when "plugins/modules/" is in the file path

--- UltiSnips/generate.py
+++ UltiSnips/generate.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
         all_modules_paths = get_files(include_user=True)
         user_modules_paths = []
         for f in all_modules_paths:
-            if 'ansible_collections' in f:
+            if 'plugins/modules/' in f:
                 user_modules_paths.append(f)
 
         user_modules_docstrings = []
